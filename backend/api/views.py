1f03d2bd66828567ac6c67b5c95ad7fc113248a7
Gabriele <briello2001@gmail.com>
2024-05-25 17:16:32 +0200

    Implement EmailView for storing and sending emails

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics
+from .models import Email
+from .serializers import EmailSerializer
+from django.core.mail import send_mail
+from django.conf import settings
 
-# Create your views here.
+
+class EmailView(generics.ListCreateAPIView):
+  queryset = Email.objects.all()
+  serializer_class = EmailSerializer
+
+  def post(self, request):
+    serializer = EmailSerializer(data=request.data)
+    if serializer.is_valid():
+      receipent = serializer.validated_data['recipient']
+      subject = serializer.validated_data['subject']
+      body = serializer.validated_data['body']
+      send_mail(
+        subject,
+        body,
+        settings.EMAIL_HOST_USER, 
+        [receipent],
+        fail_silently=False
+      )
+      return Response('Email sent successfully', status=200)
+    else:
+      print(serializer.errors)
+      return Response(serializer.errors, status=400)
+
+
+
+    
