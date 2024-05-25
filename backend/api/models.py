1687638e2c8556b587dda75b3db6ab767f15439c
Gabriele <briello2001@gmail.com>
2024-05-25 17:15:17 +0200

    Add Email model and serializer

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Email(models.Model):
+  sender = models.CharField(max_length=200, default=settings.EMAIL_HOST_USER)
+  recipient = models.CharField(max_length=200, default='')
+  subject = models.CharField(max_length=200)
+  body = models.TextField()
+  date = models.DateTimeField(auto_now_add=True)
