8e9d865b64e4eb381f3e2f287a562acfe36d51b2
Gabriele <briello2001@gmail.com>
2024-05-27 00:25:43 +0200

    Make table responsive

--- frontend/src/pages/EmailHistory.jsx
+++ frontend/src/pages/EmailHistory.jsx
@@ -4,20 +4,22 @@ const EmailHistory = ({emails, onDelete}) => {
   return(
     <div className='container'>
       <h1>Email inviate</h1>
-      <table className='table'>
-        <thead>
-          <tr>
-            <th scope='col'>Desinatario</th>
-            <th scope='col'>Oggetto</th>
-            <th scope='col'>Contenuto</th>
-            <th scope='col'>Allegato</th>
-            <th scope='col'>Inviato</th>
-          </tr>
-        </thead>
-        <tbody>
-          {emails.map((email) => <Email email={email} onDelete={onDelete} key={email.id}/>)}
-        </tbody>
-      </table>
+      <div className='table-responsive'>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th scope='col'>Desinatario</th>
+              <th scope='col'>Oggetto</th>
+              <th scope='col'>Contenuto</th>
+              <th scope='col'>Allegato</th>
+              <th scope='col'>Inviato</th>
+            </tr>
+          </thead>
+          <tbody>
+            {emails.map((email) => <Email email={email} onDelete={onDelete} key={email.id}/>)}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 };
