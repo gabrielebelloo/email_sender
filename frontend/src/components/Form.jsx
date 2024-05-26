54b10bc9dbfd16d33a730a1d4092a71a0cacb898
Gabriele <briello2001@gmail.com>
2024-05-26 12:44:29 +0200

    Combine multiple states into a single state object. Add functions to handle input changes and reset the component.

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -2,42 +2,71 @@ import {useState} from 'react';
 import api from '../api';
 
 const Form = ({ route }) => {
-  const [sender, setSender] = useState('bellogabriele2001@gmail.com');
-  const [recipient, setRecipient] = useState('');
-  const [subject, setSubject] = useState('');
-  const [body, setBody] = useState('');
+  const [email, setEmail] = useState({
+    sender: 'bellogabriele2001@gmail.com',
+    recipient: '',
+    subject: '',
+    body: ''
+  });
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     setLoading(true);
     e.preventDefault();
     try {
-      const res = await api.post(route, {recipient, subject, body})
+      const res = await api.post(route, {
+        recipient: email.recipient,
+        subject: email.subject, 
+        body: email.body
+      });
     } catch (error) {
       console.log(error)
     } finally { 
+      resetComponent();
       setLoading(false)
+      alert('Email inviata!')
     }
+  }
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setEmail({
+      ...email,
+      [name]: value
+    });
+  }
+
+  const resetComponent = () => {
+    setEmail({
+      sender: 'bellogabriele2001@gmail.com',
+      recipient: '',
+      subject: '',
+      body: ''
+    });
   }
 
   return(
     <form onSubmit={handleSubmit}>
-      <input type='text' placeholder='Sender' value={sender} readOnly/>
+      <input type='text' placeholder='Mittente' value={email.sender} readOnly/>
       <input 
+        name='recipient'  
         type='text'
         placeholder='Destinatario'
-        value={recipient}
-        onChange={(e) => setRecipient(e.target.value)}
+        value={email.recipient}
+        onChange={(e) => handleChange(e)}
       />
       <input 
+        name='subject'
         type='text'
         placeholder='Oggetto'
-        value={subject}
-        onChange={(e) => setSubject(e.target.value)}
+        value={email.subject}
+        onChange={(e) => handleChange(e)}
       />
       <textarea 
-        value={body}
-        onChange={(e) => setBody(e.target.value)}
+        name='body'
+        value={email.body}
+        onChange={(e) => handleChange(e)}
       >
       </textarea>
       <button type='submit'>Invia</button>
