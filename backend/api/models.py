27952ed3aa040ae0d4746dfaa4ec285a4230604b
Gabriele <briello2001@gmail.com>
2024-05-26 22:34:30 +0200

    Adjust models rule

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,8 +3,8 @@ from django.conf import settings
 
 class Email(models.Model):
   sender = models.CharField(max_length=200, default=settings.EMAIL_HOST_USER)
-  recipient = models.CharField(max_length=200, default='')
-  subject = models.CharField(max_length=200)
+  recipient = models.CharField(max_length=200)
+  subject = models.CharField(max_length=200, blank=True)
   body = models.TextField()
   date = models.DateTimeField(auto_now_add=True)
-  attachment = models.FileField(upload_to=settings.FILE_PATH_FIELD_DIRECTORY)
+  attachment = models.FileField(upload_to=settings.FILE_PATH_FIELD_DIRECTORY, null=True, blank=True)
