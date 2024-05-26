906dfc1540d9a3e62aaf2125c4b462e5c35d6cee
Gabriele <briello2001@gmail.com>
2024-05-26 16:28:51 +0200

    Adjust date and attachment formatting

--- frontend/src/components/Email.jsx
+++ frontend/src/components/Email.jsx
@@ -1,9 +1,20 @@
 const Email = ({ email }) => {
   const options = {
-
+    weekday: "long",
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric"
   }
 
   const formattedDate = new Date(email.date).toLocaleDateString('it-IT', options); 
+
+  const attachment = email.attachment
+  const baseUrl = import.meta.env.VITE_API_URL
+  const pathUrl = import.meta.env.VITE_FILE_PATH_DIRECTORY
+  const fullString = baseUrl + '/' + pathUrl 
+  const fileName = attachment ? (email.attachment).replace(fullString, '') : ''
 
  return(
   <tr>
@@ -11,6 +22,7 @@ const Email = ({ email }) => {
     <td>{email.recipient}</td>
     <td>{email.subject}</td>
     <td>{email.body}</td>
+    <td>{fileName}</td>
     <td>{formattedDate}</td>
   </tr>
  )
