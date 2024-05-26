52afd1538a84b1b0bd3d0a7c780903e16560c527
Gabriele <briello2001@gmail.com>
2024-05-26 22:36:28 +0200

    Import bootstrap

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './styles/style.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
